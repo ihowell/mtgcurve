d1bd7368e095ceaea4b4cf523e6816d331a96ca1
Ian Howell <ianh413@gmail.com>
2021-03-17 20:01:38 -0500

    Ready for v0.1.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='mtgcurve',
-    version='0.1.0',
+    version='0.1.1',
     description='Probability estimator for being on curve in mtg',
     long_description=long_description,
     long_description_content_type='text/markdown',
