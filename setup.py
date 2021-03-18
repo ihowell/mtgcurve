4720b302db2efec1135f1829c33cb2ef4366085d
Ian Howell <ianh413@gmail.com>
2021-03-18 11:57:28 -0500

    Updated version.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='azusa',
-    version='0.1.1',
+    version='0.1.2',
     description='Probability estimator for being on curve in mtg',
     long_description=long_description,
     long_description_content_type='text/markdown',
