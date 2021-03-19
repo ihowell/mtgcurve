c68ef830bb1eaf84669bc2d1793008cef2806db6
Ian Howell <ianh413@gmail.com>
2021-03-18 22:15:24 -0500

    Get correct number of cards in library.

--- azusa/parse.py
+++ azusa/parse.py
@@ -3,6 +3,7 @@ import fire
 
 from .mana_producers import PRODUCERS
 
+
 def parse_moxfield_url(moxfield_url):
     deck_id = moxfield_url.split('/')[-1]
     url = f'https://api.moxfield.com/v2/decks/all/{deck_id}'
@@ -13,16 +14,17 @@ def parse_moxfield_url(moxfield_url):
 
     mainboard = data['mainboard']
 
-
     mana_producers = []
     num_lands = 0
     max_cmc = 0
+    num_cards_in_library = 0
     for card, card_data in mainboard.items():
         if card in PRODUCERS:
             mana_producers.append(PRODUCERS[card])
         elif 'Land' in card_data['card']['type_line']:
             num_lands += card_data['quantity']
         max_cmc = max(max_cmc, card_data['card']['cmc'])
+        num_cards_in_library += card_data['quantity']
 
     max_cmc = int(max_cmc)
 
@@ -33,7 +35,7 @@ def parse_moxfield_url(moxfield_url):
     for i, producer in enumerate(mana_producers):
         print('\t', i, producer.name)
 
-    return len(mainboard), mana_producers, num_lands, max_cmc
+    return num_cards_in_library, mana_producers, num_lands, max_cmc
 
 
 if __name__ == '__main__':
